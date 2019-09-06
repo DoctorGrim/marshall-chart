715986c536117587c868eed5d80cd33c6eaad299
DoctorGrim <estlund.marshall@gmail.com>
2019-09-06 17:09:30 -0400

    code clean up and minor styleing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import { Bar } from "react-chartjs-2";
 import * as jsonData from "./graph_data.json";
-
-const styles = { position: "relitive", width: 1200, hight: 1000 };
-
-const makeArray = key => {
-  let arry = jsonData.default.map(function(e) {
-    return e[key];
-  });
-  return arry;
-};
-
-let labels = makeArray("label");
-let values = makeArray("value");
-let colors = makeArray("color");
+import * as styles from "./App.css"
 
 function App() {
-  const [chartData, setChartData] = useState({
+  const makeArray = key => jsonData.default.map( element => element[key])
+
+  let labels = makeArray("label");
+  let values = makeArray("value");
+  let colors = makeArray("color");
+
+  let chartData = {
     labels: labels,
     datasets: [
       {
-        label: "",
         backgroundColor: colors,
         data: values
       }
     ]
-  });
+  };
+
   return (
     <div style={styles} className="App">
       <Bar
         options={{
           response: true,
-          title: { display: true, text: "Call Type Report" },
+          title: { display: true, text: "Call Type Report", fontSize: 24 },
           legend: { display: false },
           scales: {
             yAxes: [
