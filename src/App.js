2baa3bb9bc20e7bf59eee6288ae1ca8cf5c69b89
DoctorGrim <estlund.marshall@gmail.com>
2019-09-06 16:04:42 -0400

    bar graph with axis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Bar } from "react-chartjs-2";
+import * as jsonData from "./graph_data.json";
+
+const styles = { position: "relitive", width: 1200, hight: 1000 };
+
+const makeArray = key => {
+  let arry = jsonData.default.map(function(e) {
+    return e[key];
+  });
+  return arry;
+};
+
+let labels = makeArray("label");
+let values = makeArray("value");
+let colors = makeArray("color");
 
 function App() {
+  const [chartData, setChartData] = useState({
+    labels: labels,
+    datasets: [
+      {
+        label: "",
+        backgroundColor: colors,
+        data: values
+      }
+    ]
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles} className="App">
+      <Bar
+        options={{
+          response: true,
+          title: { display: true, text: "Call Type Report" },
+          legend: { display: false },
+          scales: {
+            yAxes: [
+              {
+                scaleLabel: {
+                  display: true,
+                  labelString: "Counts"
+                }
+              }
+            ],
+            xAxes: [
+              {
+                scaleLabel: {
+                  display: true,
+                  labelString: "Call Type"
+                }
+              }
+            ]
+          }
+        }}
+        data={chartData}
+      />
     </div>
   );
 }
